f605f8990e59858cceeca66705a5064b3dce8216
ibaikaa <ibaikaaaa@gmail.com>
2023-12-10 22:56:52 +0600

    Initial Commit

---
+++ tic-tac-toe-app/tic_tac_toe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toe_appApp.swift
+//  tic-tac-toe-app
+//
+//  Created by Ian Baikuchukov on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
